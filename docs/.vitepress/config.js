fbde01d84da6f492b8d9727798a8aa0d8ceeb2ab
wzx <202320062@hdu.edu.cn>
2022-10-12 21:05:29 +0800

    deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,43 +1,50 @@
 export default {
-	base: './',
+	base: '/zx-blog/',
 	title: 'zx-blog', //站点标题
 	description: '记录一下本人的学习过程', //mate标签description，多用于搜索引擎抓取摘要
+	lastUpdated: true,
+	markdown: {
+		lineNumbers: true, //显示代码行数
+	},
 	themeConfig: {
 		siteTitle: 'ZX的博客',
 		// logo: "/logo.png",
 		nav: [{ text: 'github', link: 'https://github.com/magnum-zx' }],
 		socialLinks: [{ icon: 'github', link: 'https://github.com/magnum-zx' }],
-		sidebar: [
-			{
-				text: 'JS手写题',
-				items: [
-					{
-						text: '实现Promise',
-						link: 'articles/JS手写题/Promise',
-					},
-					{
-						text: '实现Promise类方法',
-						link: 'articles/JS手写题/PromiseClassFunction',
-					},
-					{
-						text: '实现发布订阅模式',
-						link: 'articles/JS手写题/发布订阅模式',
-					},
-					{
-						text: '实现函数柯里化',
-						link: 'articles/JS手写题/柯里化',
-					},
-					{
-						text: '实现数组拍平',
-						link: 'articles/JS手写题/数组拍平',
-					},
-				],
-			},
-			// {
-			// 	text: 'example',
-			// 	items: [
-			// 	],
-			// },
-		],
+		sidebar: getSidebar(),
 	},
+}
+
+function getSidebar() {
+	return [
+		{
+			text: 'JS手写题',
+			items: [
+				{
+					text: 'Introduction',
+					link: '/articles/jsCode/',
+				},
+				{
+					text: '实现Promise',
+					link: '/articles/jsCode/Promise',
+				},
+				{
+					text: '实现Promise类方法',
+					link: '/articles/jsCode/PromiseClassFunction',
+				},
+				{
+					text: '实现发布订阅模式',
+					link: '/articles/jsCode/发布订阅模式',
+				},
+				{
+					text: '实现函数柯里化',
+					link: '/articles/jsCode/柯里化',
+				},
+				{
+					text: '实现数组拍平',
+					link: '/articles/jsCode/数组拍平',
+				},
+			],
+		},
+	]
 }
